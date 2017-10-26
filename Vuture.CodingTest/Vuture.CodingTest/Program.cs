9fce6297adfc949333eee6080570e180e9c20898
PennyMou <panagiota_mouratidou@hotmail.com>
2017-10-26 18:24:29 +0100

    Project + Bonus Question at the end! - program.cs + util.cs

--- Vuture.CodingTest/Vuture.CodingTest/Program.cs
+++ Vuture.CodingTest/Vuture.CodingTest/Program.cs
@@ -10,7 +10,74 @@ namespace Vuture.CodingTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            bool Continue = true;
+
+            while (Continue)
+            {
+                Console.WriteLine("Please select Task (ex. 1, 2, 3A, 3B, 3C): ");
+                string task_selection = Console.ReadLine();
+
+                task_selection = Util.CheckTaskSelection(task_selection);
+
+                if (task_selection == "1")
+                {
+                    Console.WriteLine("Please give a string: ");
+                    string str = Console.ReadLine();
+                    Console.WriteLine("Please give a letter: ");
+
+                    //The method accepts a string BUT only checks the occurencies of the 1st letter of the string!
+                    char[] let = (Console.ReadLine()).ToCharArray();
+
+                    int num = Util.Task1(str, let[0]);
+                    Console.WriteLine("The number of occurencies of the letter {0} is: {1}", let[0], num);
+
+                }
+                else
+                {
+                    if (task_selection == "2")
+                    {
+                        Console.WriteLine("Please give a string: ");
+                        string str = Console.ReadLine();
+
+                        bool palindrome = Util.Task2(str);
+                        Console.WriteLine("Is the given string palindrome? Answer: {0}", palindrome);
+                    }
+                    else
+                    {
+
+                        if (task_selection == "3A" || task_selection == "3a")
+                        {
+                            Console.WriteLine("Please give a string for searching for censored words: ");
+                            string str = Console.ReadLine();
+
+                            Util.Task3A(str);
+                        }
+                        else
+                        {
+
+                            if (task_selection == "3B" || task_selection == "3b")
+                            {
+                                Console.WriteLine("Please give a string for replacing the censored words: ");
+                                string str = Console.ReadLine();
+
+                                Util.Task3B(str);
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Please give a string for replacing the palindrome words: ");
+                                string str = Console.ReadLine();
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("Do you want to continue with another Task (Y or N)?");
+                string cont=Console.ReadLine();
+                Continue = Util.IsContinue(cont);
+            }
+
+            Console.WriteLine("BONUS ANSWER! We could have the censored word list stored: \n -in a text file \n -in a database \n -in a List<> \n -in a Dictionary<>");
+            Console.ReadLine();
         }
     }
 }
---
+++ Vuture.CodingTest/Vuture.CodingTest/Util.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Vuture.CodingTest
+{
+    class Util
+    {
+        private static string[] censored_words = { "dog", "cat", "large" };
+        public static string CheckTaskSelection(string a)
+        {
+            while (a!="1" && a!="2" && a != "3A" && a != "3a" && a != "3B" && a != "3b" && a != "3C" && a != "3c")
+            {
+                Console.WriteLine("Please make a correct task selection (e.x. 1, 2, 3A, 3B, 3C): ");
+                a = Console.ReadLine();
+            }
+
+            return a;
+        }
+
+        public static int Task1(string str,char letter)
+        {
+            int num = str.Count(x => x == letter);
+            return num;
+        }
+
+        public static bool Task2(string str)
+        {
+            bool b;
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string str_reversed = new string(arr);
+            return b = (str.Equals(str_reversed)) ? true : false;
+
+
+        }
+
+        public static void Task3A(string str)
+        {
+            //string[] censored_words = { "dog", "cat", "large" };
+            int total = 0;
+
+            foreach (string k in censored_words)
+            {
+                //Regular Expressions
+                int num=(Regex.Matches(Regex.Escape(str), k)).Count;
+                Console.WriteLine("Censored word {0}: {1} times", k, num);
+                total = total + num;
+            }
+
+            Console.WriteLine("Total: {0}", total);
+        }
+
+        public static void Task3B(string str)
+        {
+            StringBuilder str2 = new StringBuilder(str);
+            
+            foreach (string k in censored_words)
+            {
+                str2 = str2.Replace(k, "$$$");
+            }
+
+            Console.WriteLine(str2);
+        }
+
+        public static void Task3C(string str)
+        {
+            string[] words=str.Split(' ');
+            StringBuilder str2 = new StringBuilder(str);
+
+            foreach (string k in words)
+            {
+                bool IsPalindrome=Task2(k);
+
+                if (IsPalindrome)
+                {
+                    str2 = str2.Replace(k, "$$$");
+                }
+            }
+
+            Console.WriteLine(str2);
+        }
+
+        public static bool IsContinue(string cont)
+        {
+            while (!(cont.Equals("Y")) && !(cont.Equals("N")) && !(cont.Equals("y")) && !(cont.Equals("n")))
+            {
+                Console.WriteLine("Please give a correct answer (Y or N): ");
+                cont = Console.ReadLine();
+            }
+
+            if (cont.Equals("Y") || cont.Equals("y"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
